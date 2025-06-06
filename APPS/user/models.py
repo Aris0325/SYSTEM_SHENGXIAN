d0296cbfcc0910a21ae5cf055e116fe29a85726a
ARIS0325 <14129499+aris0325@user.noreply.gitee.com>
2025-06-06 16:27:25 +0800

    第二次修改-qf

---
+++ APPS/user/models.py
@@ -0,0 +1,22 @@
+
+# Create your models here.
+from django.db import models
+
+# Create your models here.
+
+class UserInfo(models.Model):
+
+    IDENTITY_ITEMS = (
+        (0, '客户'),
+        (1, '采购部'),
+        (2, '物流部'),
+        (3, '仓库部'),
+        (4, '销售部'),
+        (5, '售后部'),
+    )
+    username = models.CharField(max_length=20, verbose_name='用户名')
+    pwd = models.CharField(max_length=20, verbose_name='密码')
+    mail = models.EmailField(verbose_name='邮箱')
+    tel = models.PositiveBigIntegerField(verbose_name='电话号码')
+    identity = models.PositiveIntegerField(default=0, choices=IDENTITY_ITEMS, verbose_name="部门")
+    staff_id = models.CharField(max_length=20,default=0, verbose_name='员工编号')
