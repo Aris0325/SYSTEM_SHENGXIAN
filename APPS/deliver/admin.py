9442c660650270dbfe8043264600119cfd742828
ARIS0325 <14129499+aris0325@user.noreply.gitee.com>
2025-06-07 12:39:41 +0800

    第三次修改，数据库完成-qf

--- APPS/deliver/admin.py
+++ APPS/deliver/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import DeliveryTasks
+from .models import DeliveryItems
+from .models import TemperatureLog
+from .models import DispatchOrders
 # Register your models here.
+admin.site.register(DeliveryTasks)
+admin.site.register(DeliveryItems)
+admin.site.register(TemperatureLog)
+admin.site.register(DispatchOrders)
